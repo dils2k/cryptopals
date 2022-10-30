6bdc0bd0d52085aec9933f2f8336f7555c81090e
Dils Matchanov <dils.matchanov@gmail.com>
2022-10-31 00:34:22 +0500

    CBC encryption

--- set2.go
+++ set2.go
@@ -1,6 +1,11 @@
 package cryptopals
 
-func PKCSPadding(msg []byte, blen int) []byte {
+import (
+	"crypto/aes"
+	"errors"
+)
+
+func padding(msg []byte, blen int) []byte {
 	var res []byte
 	for i := 0; i < blen; i++ {
 		if i < len(msg) {
@@ -10,4 +15,48 @@ func PKCSPadding(msg []byte, blen int) []byte {
 		}
 	}
 	return res
+}
+
+func CBCEncrypt(msg, iv, key []byte) ([]byte, error) {
+	if len(key) != 16 {
+		return nil, errors.New("key length must be 16")
+	}
+
+	if len(iv) == 0 {
+		return nil, errors.New("iv must not be nil")
+	}
+
+	resBlocks := make([][]byte, 0)
+	blocks := chunkBy(msg, 16)
+	for i, b := range blocks {
+		if i == len(blocks)-1 {
+			b = padding(b, 16)
+		}
+		if i == 0 {
+			b = RepeatingKeyXOR(b, iv)
+		} else {
+			b = RepeatingKeyXOR(b, resBlocks[i-1])
+		}
+		resBlocks = append(resBlocks, ECBEncrypt(b, key))
+	}
+
+	var res []byte
+	for _, b := range resBlocks {
+		res = append(res, b...)
+	}
+
+	return res, nil
+}
+
+func ECBEncrypt(msg, key []byte) []byte {
+	cipher, _ := aes.NewCipher([]byte(key))
+	encrypted := make([]byte, len(msg))
+
+	size := cipher.BlockSize()
+
+	for bs, be := 0, size; bs < len(msg); bs, be = bs+size, be+size {
+		cipher.Encrypt(encrypted[bs:be], msg[bs:be])
+	}
+
+	return encrypted
 }
--- set2_test.go
+++ set2_test.go
@@ -1,9 +1,26 @@
 package cryptopals
 
-import "testing"
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
 
 func TestPKCSPadding(t *testing.T) {
-	if l := len(PKCSPadding([]byte("YELLOW SUBMARINE"), 20)); l != 20 {
+	if l := len(padding([]byte("YELLOW SUBMARINE"), 20)); l != 20 {
 		t.Fatalf("length must be 20, but got %d", l)
+	}
+}
+
+func TestCBCEncrypt(t *testing.T) {
+	res, _ := CBCEncrypt([]byte("shisui"), []byte("some"), []byte("YELLOW SUBMARINE"))
+	fmt.Println(bytes2hex(res))
+}
+
+func TestECBEncrypt(t *testing.T) {
+	msg := []byte("YELLOW SUBMARINEYELLOW SUBMARINE")
+	key := []byte("YELLOW SUBMARINE")
+	if res := ECBDecrypt(ECBEncrypt(msg, key), key); !reflect.DeepEqual(res, msg) {
+		t.Fatal("invalid result of decryption", string(res))
 	}
 }
